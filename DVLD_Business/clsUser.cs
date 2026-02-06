53bbdcb813afbdcdd2f0cefdda055f3656c05ef2
Ghaith Estaif <ghaithestaif16@gmail.com>
2026-02-06 17:34:12 +0300

    Added the User Data and Business Layer

--- DVLD_Business/clsUser.cs
+++ DVLD_Business/clsUser.cs
@@ -1,12 +1,121 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DVLD_DataAccess;
+using System.Xml.Linq;
 
-namespace DVLD_Business
+public class clsUser
 {
-    internal class clsUser
+    // Data Members (same as table, no nulls)
+    public int UserID { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public bool IsActive { get; set; }
+
+    // This is what you asked for
+    public DVLD_Business.People Person { get; set; }
+    enum enMode { Addnew, Update}
+    enMode _Mode;
+    public clsUser()
     {
+        UserID = -1;
+        UserName = "";
+        Password = "";
+        IsActive = false;
+        Person = new DVLD_Business.People();
+        _Mode=enMode.Addnew;
+    }
+
+    private clsUser(int UserID,int PersonID,
+    string UserName,
+    string Password ,
+    bool IsActive)
+    {
+       this.UserID = UserID;
+        this.UserName = "";
+        this.Password = "";
+        this.IsActive = false;
+        this.Person = DVLD_Business.People.Find(PersonID);
+        _Mode=enMode.Update;
+    }
+    // ---------- FIND ----------
+    public clsUser Find(int userID)
+    {
+      int PersonID = -1;
+        string UserName = "";
+        string Password = "";
+        bool IsActive = false;
+        if(clsUserData.Find(
+                userID,
+                ref PersonID,
+                ref UserName,
+                ref Password,
+                ref IsActive
+            ))
+        {
+            return new clsUser(userID,
+                 PersonID,
+                 UserName,
+                 Password,
+                 IsActive);
+        }
+        return null;
+  
+    }
+
+    // ---------- ADD NEW ----------
+    private int _AddNew()
+    {
+       
+          int UserID = clsUserData.AddNew(
+                Person.PersonID,
+                UserName,
+                Password,
+                IsActive
+            );
+
+        return UserID;
+       
+    }
+    private bool _Update()
+    {
+        
+            return clsUserData.Update(
+                UserID,
+                Person.PersonID,
+                UserName,
+                Password,
+                IsActive
+            );
+        
+    }
+    // ---------- DELETE ----------
+    public static bool Delete(int userID)
+    {
+       
+            return clsUserData.Delete(userID);
+    
+    }
+
+    // ---------- IS EXIST ----------
+    public static bool IsExist(int userID)
+    {
+        
+            return clsUserData.IsExist(userID);
+      
+
+    }
+
+
+    public  bool Save()
+    {
+
+        if (_Mode==enMode.Addnew)
+        {
+           int Person= _AddNew();
+            _Mode = enMode.Update;
+            return Person > 0;
+        }
+        else
+        {
+            return _Update();
+        }
     }
 }
---
+++ DVLD_DataAccess/clsUserData.cs
@@ -0,0 +1,178 @@
+﻿using DVLD_DataAccess;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+public class clsUserData
+{
+    // ---------- FIND ----------
+    public static bool Find(
+        int userID,
+        ref int PersonID,
+        ref string outUserName,
+        ref string outPassword,
+        ref bool outIsActive)
+    {
+        SqlConnection connection = new SqlConnection(AppSettings.ConnectionString);
+        SqlCommand command = new SqlCommand(
+            "SELECT * FROM Users WHERE UserID = @UserID",
+            connection);
+
+        command.Parameters.AddWithValue("@UserID", userID);
+
+        try
+        {
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+
+            if (reader.Read())
+            {
+                userID = (int)reader["UserID"];
+                outUserName = (string)reader["UserName"];
+                outPassword = (string)reader["Password"];
+                outIsActive = (bool)reader["IsActive"];
+
+                int personID = (int)reader["PersonID"];                
+
+                return true;
+            }
+
+            return false;
+        }
+        catch
+        {
+            return false;
+        }
+        finally
+        {
+            connection.Close();
+        }
+    }
+
+    // ---------- ADD NEW ----------
+    public static int AddNew(
+        int personID,
+        string userName,
+        string password,
+        bool isActive)
+    {
+        SqlConnection connection = new SqlConnection(AppSettings.ConnectionString);
+        SqlCommand command = new SqlCommand(
+            @"INSERT INTO Users (PersonID, UserName, Password, IsActive)
+              VALUES (@PersonID, @UserName, @Password, @IsActive);
+              SELECT SCOPE_IDENTITY();",
+            connection);
+
+        command.Parameters.AddWithValue("@PersonID", personID);
+        command.Parameters.AddWithValue("@UserName", userName);
+        command.Parameters.AddWithValue("@Password", password);
+        command.Parameters.AddWithValue("@IsActive", isActive);
+
+        try
+        {
+            connection.Open();
+            object result = command.ExecuteScalar();
+
+            if (result != null)
+                return Convert.ToInt32(result);
+
+            return -1;
+        }
+        catch
+        {
+            return -1;
+        }
+        finally
+        {
+            connection.Close();
+        }
+    }
+
+    // ---------- DELETE ----------
+    public static bool Delete(int userID)
+    {
+        SqlConnection connection = new SqlConnection(AppSettings.ConnectionString);
+        SqlCommand command = new SqlCommand(
+            "DELETE FROM Users WHERE UserID = @UserID",
+            connection);
+
+        command.Parameters.AddWithValue("@UserID", userID);
+
+        try
+        {
+            connection.Open();
+            return command.ExecuteNonQuery() > 0;
+        }
+        catch
+        {
+            return false;
+        }
+        finally
+        {
+            connection.Close();
+        }
+    }
+
+    // ---------- IS EXIST ----------
+    public static bool IsExist(int userID)
+    {
+        SqlConnection connection = new SqlConnection(AppSettings.ConnectionString);
+        SqlCommand command = new SqlCommand(
+            "SELECT 1 FROM Users WHERE UserID = @UserID",
+            connection);
+
+        command.Parameters.AddWithValue("@UserID", userID);
+
+        try
+        {
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            return reader.HasRows;
+        }
+        catch
+        {
+            return false;
+        }
+        finally
+        {
+            connection.Close();
+        } 
+    }
+        public static bool Update(
+           int userID,
+           int personID,
+           string userName,
+           string password,
+           bool isActive)
+        {
+               SqlConnection connection = new SqlConnection(AppSettings.ConnectionString);
+               SqlCommand command = new SqlCommand(
+                   @"UPDATE Users
+                 SET PersonID = @PersonID,
+                     UserName = @UserName,
+                     Password = @Password,
+                     IsActive = @IsActive
+                 WHERE UserID = @UserID",
+                   connection);
+               
+               command.Parameters.AddWithValue("@UserID", userID);
+               command.Parameters.AddWithValue("@PersonID", personID);
+               command.Parameters.AddWithValue("@UserName", userName);
+               command.Parameters.AddWithValue("@Password", password);
+               command.Parameters.AddWithValue("@IsActive", isActive);
+               
+               try
+               {
+                   connection.Open();
+                   return command.ExecuteNonQuery() > 0;
+               }
+               catch
+               {
+                   return false;
+               }
+               finally
+               {
+                   connection.Close();
+               }
+        }
+
+}
